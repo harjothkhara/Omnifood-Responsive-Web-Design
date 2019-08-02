4a13897928e845d7dd3a0f8b77fde253a35c97da
harjoth <harjoth.khara@gmail.com>
2019-08-01 18:03:02 -0700

    jquery again - making the navigation responsive - works except menu close jquery class does not.

--- Omnifood/resources/js/script.js
+++ Omnifood/resources/js/script.js
@@ -80,4 +80,18 @@ $('a[href*="#"]')
     offset: '50%'
   });
 
+  /* Mobile nav */
+  $('.js--nav-icon').click(function(){
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon');
+
+    nav.slideToggle(200);
+    if (icon.hasClass('menu')) {
+        icon.addClass('close');
+        icon.removeClass('menu');
+    } else{
+        icon.addClass('menu');
+        icon.removeClass('close');
+    }
+  });
 });
